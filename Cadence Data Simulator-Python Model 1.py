4ceaf3dcbbb6491828e84932c5578f643c012c29
hunter-alloway <haalloway@liberty.edu>
2022-01-24 11:26:56 -0500

    Installing Pandas

--- Cadence Data Simulator-Python Model 1.py
+++ Cadence Data Simulator-Python Model 1.py
@@ -3,23 +3,26 @@
 # Created On: 2022-01-24 14:31:46.415580
 
 print ("Hello World")
-try: 
-    f = open("airlines.csv", encoding = 'utf-8')
+# try: 
+#    f = open("airlines.csv", encoding = 'utf-8')
     #perform file operations
-finally: 
-    f.close()
+#finally: 
+#    f.close()
 
-import csv
+# import time
+# gmt = time.gmtime(time.time())
 
-from datetime import datetime
-now = datetime.now()
-print( now)
-print('Type:', type(now))
+# import csv
+# with open('airtravel.csv', 'w', newline = '') as file:
+   #  writer = csv.writer(file)
+  #  writer.writerow(["Time", "ICCID (SIM ID)", "NodeID", "Connection Event", "Bytes Used"])
+   # writer.writerow(["Time", 1111111111, "A000001", "Connect", "0-bytes"])
+   # writer.writerow(["time", 2222222222, "A000002", "Connect", "0-bytes"])
+  #  writer.writerow(["Timestamp in GMT", 1111111111, "A000001", "Disconnect", "2000-bytes"])
+  #  writer.writerow(["Timestamp in GMT", 2222222222, "A000002", "Disconnect", "2000-bytes"])
 
-with open('airtravel.csv', '', newline = '') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Time", "ICCID (SIM ID)", "NodeID", "connection event", "bytes used"])
-    writer.writerow(["Timestamp in GMT", 1111111111, "A000001", "Connect", "0-bytes"])
-    writer.writerow(["Timestamp in GMT", 2222222222, "A000002", "Connect", "0-bytes"])
-    writer.writerow(["Timestamp in GMT", 1111111111, "A000001", "Disconnect", "2000-bytes"])
-    writer.writerow(["Timestamp in GMT", 2222222222, "A000002", "Disconnect", "2000-bytes"])
+#run in command prompt if running code for the first time: pip install pandas --user
+
+import pandas
+df = pandas.read_csv("/Users/Hunter/Documents/Liberty University/Fourth Year/Classes/Spring 2022/ENGR 482/Visual Studios/Cadence Documentation/CSV/airtravel.csv", index_col='NodeID')
+print(df)
