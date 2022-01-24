c9f1c6afc6437b4818ca87aacda7b87e3a6ed492
hunter-alloway <haalloway@liberty.edu>
2022-01-24 09:35:11 -0500

    Incorporating GMT into CSV Generation

--- Cadence Data Simulator-Python Model 1.py
+++ Cadence Data Simulator-Python Model 1.py
@@ -1,3 +1,7 @@
+# Hunter Alloway
+#Cadence-Data Simulator
+# Created On: 2022-01-24 14:31:46.415580
+
 print ("Hello World")
 try: 
     f = open("airlines.csv", encoding = 'utf-8')
@@ -6,8 +10,16 @@ finally:
     f.close()
 
 import csv
-with open('airtravel.csv', 'w', newline = '') as file:
+
+from datetime import datetime
+now = datetime.now()
+print( now)
+print('Type:', type(now))
+
+with open('airtravel.csv', '', newline = '') as file:
     writer = csv.writer(file)
-    writer.writerow(["Time", "ICCID (SIM ID)", "random field", "connection event", "bytes used"])
-    writer.writerow(["Timestamp in GMT", 2222222222, "xyz", "connect", 0])
-    
+    writer.writerow(["Time", "ICCID (SIM ID)", "NodeID", "connection event", "bytes used"])
+    writer.writerow(["Timestamp in GMT", 1111111111, "A000001", "Connect", "0-bytes"])
+    writer.writerow(["Timestamp in GMT", 2222222222, "A000002", "Connect", "0-bytes"])
+    writer.writerow(["Timestamp in GMT", 1111111111, "A000001", "Disconnect", "2000-bytes"])
+    writer.writerow(["Timestamp in GMT", 2222222222, "A000002", "Disconnect", "2000-bytes"])
