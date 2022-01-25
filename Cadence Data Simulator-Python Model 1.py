c8b20d98969e3adb980e1e95d3bf52c96496aa99
hunter-alloway <haalloway@liberty.edu>
2022-01-25 00:13:52 -0500

    Generated CSV

--- Cadence Data Simulator-Python Model 1.py
+++ Cadence Data Simulator-Python Model 1.py
@@ -2,6 +2,8 @@
 #Cadence-Data Simulator
 # Created On: 2022-01-24 14:31:46.415580
 
+# Note-Run the following in command prompt if running this code for the first time: "pip install pandas --user"
+
 print ("Hello World")
 # try: 
 #    f = open("airlines.csv", encoding = 'utf-8')
@@ -12,17 +14,23 @@ print ("Hello World")
 # import time
 # gmt = time.gmtime(time.time())
 
-# import csv
-# with open('airtravel.csv', 'w', newline = '') as file:
-   #  writer = csv.writer(file)
-  #  writer.writerow(["Time", "ICCID (SIM ID)", "NodeID", "Connection Event", "Bytes Used"])
-   # writer.writerow(["Time", 1111111111, "A000001", "Connect", "0-bytes"])
-   # writer.writerow(["time", 2222222222, "A000002", "Connect", "0-bytes"])
-  #  writer.writerow(["Timestamp in GMT", 1111111111, "A000001", "Disconnect", "2000-bytes"])
-  #  writer.writerow(["Timestamp in GMT", 2222222222, "A000002", "Disconnect", "2000-bytes"])
-
-#run in command prompt if running code for the first time: pip install pandas --user
+import csv
+with open('/Users/Hunter/Documents/Liberty University/Fourth Year/Classes/Spring 2022/ENGR 482/Visual Studios/Cadence Documentation/CSV/simulator.csv', 'w', newline = '') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Time", 1111111111, "A000001", "Connected", "0-bytes"])
+    writer.writerow(["Time", 2222222222, "A000002", "Connected", "0-bytes"])
+    writer.writerow(["Time", 1111111111, "A000001", "Disconnected", "2000-bytes"])
+    writer.writerow(["This time is a test", 2222222222, "A000002", "Disconnected", "2000000-bytes"])
 
 import pandas
-df = pandas.read_csv("/Users/Hunter/Documents/Liberty University/Fourth Year/Classes/Spring 2022/ENGR 482/Visual Studios/Cadence Documentation/CSV/airtravel.csv", index_col='NodeID')
+df = pandas.read_csv("/Users/Hunter/Documents/Liberty University/Fourth Year/Classes/Spring 2022/ENGR 482/Visual Studios/Cadence Documentation/CSV/simulator.csv",
+index_col='NodeID', 
+header=0,
+names=['Time', 'ICCID(SIM ID)', 'NodeID', 'Connection', 'Bytes Used'])
+df.to_csv('/Users/Hunter/Downloads/simulator_modified.csv')
 print(df)
+
+# for x in df:
+ #  print (x)
+  #if x == 'break':
+ #   break
