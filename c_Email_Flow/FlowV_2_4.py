c25894280e7da3237b0adc92e088014b6a7cf552
hunter-alloway <haalloway@liberty.edu>
2022-04-03 22:13:07 -0400

    Now includes the attached PDF Visualized Dashboard Report from PowerBI

--- c_Email_Flow/FlowV_2_4.py
+++ c_Email_Flow/FlowV_2_4.py
@@ -50,13 +50,14 @@ def sendTheMail(outlook, emailAddr, emailSubject, documentPath, ArchiveFolder, n
 
     print(documentPath)
     mail.Attachments.Add(documentPath)
-    mail.Attachments.Add(parentLocation + '/cadence_pbi_v2_2.pdf')
+
     #print("Attaching:                    ", documentPath)
     print("ATTACHED")
     time.sleep(1)
 
     # send mail!
     mail.Send()
+    
     #print("File has been sent successfully")
     print("EMAILED")
     #schedule.run_all()
@@ -71,8 +72,8 @@ def sendTheMail(outlook, emailAddr, emailSubject, documentPath, ArchiveFolder, n
     print("MOVED OUT OF ACTIVE DIRECTORY")
     
     print("\n")
-    del mail
-
+    del mail 
+    
 # GENERATE HEARTBEAT EVERY 10 minutes
 def heartbeat():
     EmailFlowHeartbeat.writerow([datetime.now().strftime("%Y-%m-%d_%H%M%S"), "Working", int(filecount)])
@@ -165,13 +166,20 @@ with open(directory + '/' + heartbeatFileName + '.csv', 'w+', newline = '') as f
                 for documentPath in filesim:
                     sendTheMail(outlook, emailAddr, emailSubject, documentPath, ArchiveFolder, newdirectory)
                     print("Some heartbeat file(s) now sent successfully")
-                
+
             print("This has closed with keyboard interference.\n\n")                
+
+# Sending a PDF of the Visualized Dashboard Report
+    if os.path.isfile(parentLocation + '/cadence_pbi_v2_2.pdf'):
+        sendTheMail(outlook, emailAddr, emailSubject, parentLocation + '/cadence_pbi_v2_2.pdf', ArchiveFolder, newdirectory)
+        print('The visualized dashboard has been successfully attached') 
+    else: 
+        print('The visual dashboard report has already been attached')
 
 # Finally, send the current Email Flow Heartbeat file: close the file, then email
 file1.close()
 
-print("=== NOW SENDING CURRENT EmailFlow HEARTBEAT FILE ===")
+print("=== NOW SENDING CURRENT EmailFlow HEARTBEAT FILE AND VISUALIZED DASHBOARD REPORT===")
 sendTheMail(outlook, emailAddr, emailSubject, directory+"/"+heartbeatFileName+ '.csv', ArchiveFolder, newdirectory)
 
 print("===================================\nWoot! End of program!")
