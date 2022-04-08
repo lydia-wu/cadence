20054abe806f0aa2413ff8b77f38365b894b576f
Hayley Yukihiro <75403138+htyuki@users.noreply.github.com>
2022-04-08 11:26:22 -0400

    Add files via upload

---
+++ d_postprocessing/archive_old.py
@@ -0,0 +1,47 @@
+# last edited by Michael Di Girolamo at 3/30/22 7:45 PM
+
+import re
+import os
+import glob
+import shutil
+import schedule
+import time
+from datetime import datetime
+
+#user = input("Hello, thank you for using the Cadence Archive Tool. Please provide your username (For reference, username would reside within this structure /Users/tsuru/OneDrive/): ")
+user = 'lydia'
+
+def archive_old():
+    old_path = 'C:/Users/' + user + '/Liberty University/Group-Cadence Data Simulator-Document Platform - Documents/Data Simulator Offload/' # file path where old files are located
+    arch_path = 'Archived/' # file path to where the old files will be archived
+    old_ext = '.zip'        # extension of old files (include the '.' i.e. -> '.zip')
+    days_old = 23           # days until file is archived
+    curr_date = datetime.now()
+
+    os.chdir(old_path)          # changes directory to defined file path
+    for file in glob.glob("*" + old_ext):
+        file_res = re.findall("DeviceLog_(\d+)-(\d+)-(\d+)_", file) # grabs the date from filename
+        if not file_res: continue
+        file_year, file_month, file_day = file_res[0]
+        file_string = file_year + ' ' + file_month + ' ' + file_day
+        file_date = datetime.strptime(file_string, "%Y %m %d")      # turns the date info into a datetime object
+
+        # Checks if file is old
+        days_between = (curr_date - file_date).days
+        if days_between > days_old:
+            print(file + " ** File is " + str(days_between) + " days old\n")
+            srcpath = file
+            destpath = arch_path + file
+            shutil.move(srcpath, destpath)
+
+# Run the script
+schedule.every().day.at("01:00").do(archive_old)
+#schedule.every().day.at("19:56").do(archive_old)    # for testing purposes
+
+while True:
+    schedule.run_pending()
+    time.sleep(10) # wait one minute
+            
+
+
+    
---
+++ d_postprocessing/lookupTable_cust.py
@@ -0,0 +1,112 @@
+# Hayley Yukihiro
+# Lookup Table Class
+# Created On: 2022-03-10 23:11:19
+# Last updated by: Hayley Yukihiro, 2022-03-25 12:31:00
+# Last updated by: L. Wu, 2022-03-31 21:32:00
+
+import csv
+import pandas as pd
+import os
+from os import listdir
+from os.path import isfile, join
+import re
+import pandas as pd
+
+#user = input("Hello, thank you for using the Cadence Lookup Table. Please provide your username (For reference, username would reside within this structure /Users/tsuru/OneDrive/): ")
+user = 'lydia'
+print("Look Up Table is Processing")
+
+# Establishes lookup table
+df = pd.read_csv('/Users/' + user + '/Liberty University/Group-Cadence Data Simulator-Document Platform - Documents/Customer Data Offload/node_id__to__sim_id_lookup_manifest_3-30-2022.csv')
+node = df['NodeID']
+sim = df['ICCID(SIM)#']
+#print(node)
+#print(sim)
+lookupNode = {}
+lookupSIM = {}
+
+updatedFile = set()
+
+# Lookup SIMID from given NodeID
+for x in range(node.size):
+    lookupSIM[node[x]] = sim[x]
+def findSIM(node):
+    return lookupSIM.get(node)
+    #if (lookupSIM[node]):
+    #    return lookupSIM[node]
+    #else:
+    #    return "DNE in Manifest"
+
+#print("Here is the lookupSIM result:\n")
+#print(lookupSIM)
+#print("==============================\n\n")
+
+# Lookup NodeID from given SIMID
+for x in range(sim.size):
+    lookupNode[sim[x]] = node[x]
+def findNode(sim):
+    return lookupNode.get(sim)
+
+#print("Here is the lookupNODE result:\n")
+#print(lookupNode)
+#print("==============================\n\n")
+
+# Populate SIM IDs
+def fillSIM(file):
+    filesize = os.path.getsize(file)
+    if filesize < 1000:
+        return
+    app = pd.read_csv(file)
+    if app.shape[0] < 500:
+        return
+    nodeID = app['Node ID']
+    #print("Here are our NodeIDs:", nodeID)
+    simID = []
+    for x in nodeID:
+        #print(x)
+        simID.append(findSIM(x))
+        #print(simID)
+    
+    app['SIM ID'] = simID
+    app.to_csv(file, index=False)
+    updatedFile.add(file)
+
+# Populate Node IDs
+def fillNode(file):
+    filesize = os.path.getsize(file)
+    if filesize < 1000:
+        return
+    net = pd.read_csv(file)
+    if net.shape[0] < 1000:
+        return
+    simID = net['ICCID']
+    nodeID = []
+    for x in simID:
+        nodeID.append(findNode(x))
+    net['NodeID'] = nodeID
+    net.to_csv(file, index=False)
+    updatedFile.add(file)
+
+try:
+    while True:
+        onlyfiles = [f for f in listdir('/Users/' + user + '/Liberty University/Group-Cadence Data Simulator-Document Platform - Documents/Customer Data Offload/') if isfile(join('/Users/' + user + '/Liberty University/Group-Cadence Data Simulator-Document Platform - Documents/Customer Data Offload/', f))]
+        appReports = []
+        networkReports = []
+        for file in onlyfiles:
+            if re.match(r'^heartbeat-logs', file):
+                appReports.append('/Users/' + user + '/Liberty University/Group-Cadence Data Simulator-Document Platform - Documents/Customer Data Offload/'+file)
+            
+            if re.match(r'^Itemised', file):
+                networkReports.append('/Users/' + user + '/Liberty University/Group-Cadence Data Simulator-Document Platform - Documents/Customer Data Offload/'+file)
+
+        for x in appReports:
+            if x not in updatedFile:
+                #print(x, "Tada! This is the file we are working with!\n")
+                fillSIM(x)
+
+        for x in networkReports:
+            if x not in updatedFile:
+                fillNode(x)
+
+except KeyboardInterrupt:
+    print("Finished Program")
