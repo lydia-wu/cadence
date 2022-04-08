97c99ff5ad023cec0365713ceb76ce4d1acd9c6f
lydia-wu <ljwu@liberty.edu>
2022-04-08 17:22:46 -0400

    top_level batch update for client

--- top_level.bat
+++ top_level.bat
@@ -11,6 +11,8 @@ pause
 :: Generate our files
 echo We will now generate files.
 start "Data Simulator!" C:\Users\lydia\GitHubLocal\cadence\Top_Level_Processing\sim_v1_0.bat
+pause
+echo We will now connect to client.
 start "Client! test1"         C:\Users\lydia\GitHubLocal\cadence\Top_Level_Processing\client_v1_1_1st.bat
 start "Client! test2"         C:\Users\lydia\GitHubLocal\cadence\Top_Level_Processing\client_v1_1_2nd.bat
 start "Client! test3"         C:\Users\lydia\GitHubLocal\cadence\Top_Level_Processing\client_v1_1_3rd.bat
