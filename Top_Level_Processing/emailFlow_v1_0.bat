7114cab72cb7878d16c12f69ec530bf04d1f1253
lydia-wu <ljwu@liberty.edu>
2022-04-11 13:04:12 -0400

    error-checking the email flow

--- Top_Level_Processing/emailFlow_v1_0.bat
+++ Top_Level_Processing/emailFlow_v1_0.bat
@@ -1,3 +1,3 @@
 @echo off
-title Cadence EMAIL FLOW v2.4
-"C:\Users\lydia\.venv\Scripts\python.exe" "C:\Users\lydia\GitHubLocal\cadence\c_Email_Flow\FlowV_2_5.py"
+title Cadence EMAIL FLOW v2.6
+"C:\Users\lydia\.venv\Scripts\python.exe" "C:\Users\lydia\GitHubLocal\cadence\c_Email_Flow\FlowV_2_6.py"
